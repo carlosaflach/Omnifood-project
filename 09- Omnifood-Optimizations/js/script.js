788c61012e9d254ffd7c49a957c63d8a56a55605
Carlos Afonso <carlos.flach15@gmail.com>
2022-05-30 10:36:55 -0300

    Making  the Mobile Navigation Work.

--- 09- Omnifood-Optimizations/js/script.js
+++ 09- Omnifood-Optimizations/js/script.js
@@ -1,8 +1,19 @@
 'use strict'
 
+// Set Current year
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open');
+});
+
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
